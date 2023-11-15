ddcfdb5537612e9b4fc2d3a999409a03bd209fa5
Juan Diego Guerra <jdxelric@gmail.com>
2023-11-15 18:54:51 -0500

    Update: routes, seeds, tailwind file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :foods
+  resources :recipes
+  resources :public_recipes, only: [:index]
+  resources :general_shopping_list, only: [:index]
 end
