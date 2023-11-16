6c3b6d6314fc2a2e1c71bf4345a876fa64df96bc
Juan Diego Guerra <jdxelric@gmail.com>
2023-11-16 16:14:06 -0500

    Fixing Rubocop Linter Errors
    Co-Authored-By: Osman Alhassan <oalhassan847@gmail.com>

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,6 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @foods = current_user.foods
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,10 @@
 class Ability
-    include CanCan::Ability
-  
-    def initialize(user)
-      return unless user.present?
-  
-      can :read, :all
-      can :manage, Recipe, user_id: user.id
-    end
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can :read, :all
+    can :manage, Recipe, user_id: user.id
   end
+end
