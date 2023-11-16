f1167369c666dc7a5bfa967656d3ec20361786ed
Juan Diego Guerra <jdxelric@gmail.com>
2023-11-16 12:24:04 -0500

    Update Controllers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,39 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.all
+  end
 
   def show; end
 
-  def create; end
+  def new
+    @recipe = Recipe.new
+    @user = current_user
+  end
 
-  def new; end
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
 
-  def destroy; end
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    respond_to do |format|
+      format.html { redirect_to recipes_url, notice: 'Recipe was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
