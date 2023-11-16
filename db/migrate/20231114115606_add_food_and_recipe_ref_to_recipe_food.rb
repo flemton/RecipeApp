9d6d67bda0e278d7a827b4e46aa0d89ad47cc368
Juan Diego Guerra <jdxelric@gmail.com>
2023-11-16 13:24:23 -0500

    Update Migrations

--- db/migrate/20231114115606_add_food_and_recipe_ref_to_recipe_food.rb
+++
@@ -1,6 +0,0 @@
-class AddFoodAndRecipeRefToRecipeFood < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :recipe_foods, :foods, null: false, foreign_key: true
-    add_reference :recipe_foods, :recipe, null: false, foreign_key: true
-  end
-end
--- db/migrate/20231114115749_add_users_ref_to_recipe.rb
+++
@@ -1,5 +0,0 @@
-class AddUsersRefToRecipe < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :recipes, :users, null: false, foreign_key: true
-  end
-end
