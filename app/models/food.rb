61181e1c9e4fa3f73b99e14df6641d857d887b81
Alhassan Osman <oalhassan847@gmail.com>
2023-11-16 21:00:53 +0000

    Add more validations to food

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,6 +5,6 @@ class Food < ApplicationRecord
   validates :name, presence: true, length: { maximum: 250, too_long: '%<count>s characters is the maximum allowed' }
   validates :measurement_unit, presence: true,
                                length: { maximum: 50, too_long: '%<count>s characters is the maximum allowed' }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true
 end
