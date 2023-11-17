0c1c5de7c2325a2e4d186cda22e6894da0dcd4e8
Alhassan Osman <oalhassan847@gmail.com>
2023-11-17 16:23:36 +0000

    Fix migrations

--- db/migrate/20231114114822_create_recipe.rb
+++ db/migrate/20231114114822_create_recipe.rb
@@ -1,4 +1,4 @@
-class CreateRecipes < ActiveRecord::Migration[7.0]
+class CreateRecipe < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :name
@@ -6,7 +6,6 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
       t.decimal :cooking_time
       t.text :description
       t.boolean :public
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -19,18 +19,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_101716) do
     t.string "measurement_unit"
     t.decimal "price"
     t.integer "quantity"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity", default: 0
-    t.bigint "food_id", null: false
-    t.bigint "recipe_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "food_id", null: false
+    t.bigint "recipe_id", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
@@ -41,9 +41,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_101716) do
     t.decimal "cooking_time"
     t.text "description"
     t.boolean "public"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
@@ -56,6 +56,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_101716) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
